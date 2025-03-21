5f3a53458ac300203478fd7985bd5a0694e939f6
LuoBin <2646887443@qq.com>
2025-03-21 17:51:40 +0800

    add inplace.py

---
+++ ch2/inplace.py
@@ -0,0 +1,17 @@
+import torch
+
+X = torch.arange(12, dtype=torch.float32).reshape(3, 4)
+Y = torch.tensor([[2.0, 1, 4, 3], [1, 2, 3, 4], [4, 3, 2, 1]])
+
+before = id(Y)
+Y = Y + X
+print(before == id(Y))
+
+Z = torch.zeros_like(Y)
+print('id(Z):', id(Z))
+Z[:] = X + Y
+print('id(Z):', id(Z))
+
+before = id(X)
+X += Y
+print(before == id(X))
