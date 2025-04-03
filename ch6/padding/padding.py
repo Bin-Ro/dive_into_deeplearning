50aafaf4825a0a70c09828ee58b558f578f21516
LuoBin <2646887443@qq.com>
2025-04-03 11:04:48 +0800

    fix typo

--- ch6/padding/padding.py
+++ ch6/padding/padding.py
@@ -4,7 +4,7 @@ from torch import nn
 def comp_conv2d(conv2d, X):
     X = X.reshape((1, 1) + X.shape)
     Y = conv2d(X)
-    return Y.reshape(X.shape[2:])
+    return Y.reshape(Y.shape[2:])
 
 conv2d = nn.Conv2d(1, 1, kernel_size=3, padding=1)
 X = torch.rand(size=(8, 8))
