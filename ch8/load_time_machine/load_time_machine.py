bf40c6ff1cad319c3084b90bc6c03ab6a1a5d152
LuoBin <2646887443@qq.com>
2025-04-21 18:13:56 +0800

    load_time_machine

---
+++ ch8/load_time_machine/load_time_machine.py
@@ -0,0 +1,117 @@
+import collections
+import re
+import hashlib
+import os
+import tarfile
+import zipfile
+import requests
+
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+DATA_HUB['time_machine'] = (DATA_URL + 'timemachine.txt', '090b5e7e70c295757f55df93cb0a180b9691891a')
+
+def download(name, cache_dir='data'):
+    assert name in DATA_HUB, f'{name} does not exist in {DATA_HUB}'
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split('/')[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+def read_time_machine():
+    with open(download('time_machine'), 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+#lines = read_time_machine()
+#print(f'len(lines): {len(lines)}')
+
+def tokenize(lines, token='word'):
+    if token == 'word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print(f'Error: Unknown token type: {token}')
+
+#tokens = tokenize(lines)
+#for i in range(11):
+#    print(tokens[i])
+
+class Vocab:
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        self.idx_to_token = ['<unk>'] + reserved_tokens
+        self.token_to_idx = {token: idx for idx, token in enumerate(self.idx_to_token)}
+
+        for token, freq in self._token_freqs:
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self):
+        return 0
+
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+
+def count_corpus(tokens):
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+#vocab = Vocab(tokens)
+#print(list(vocab.token_to_idx.items())[:10])
+#
+#for i in (0, 10):
+#    print(f'tokens[{i}]: {tokens[i]}')
+#    print(f'vocab[tokens[{i}]]: {vocab[tokens[i]]}')
+
+def load_corpus_time_machine(max_tokens=-1):
+    lines = read_time_machine()
+    tokens = tokenize(lines, 'char')
+    vocab = Vocab(tokens)
+    corpus = [vocab[token] for line in tokens for token in line]
+    if max_tokens > 0:
+        count_corpus = count_corpus[:max_tokens]
+    return corpus, vocab
+
+corpus, vocab = load_corpus_time_machine()
+print(f'len(corpus): {len(corpus)}')
+print(f'len(vocab): {len(vocab)}')
+print(f'vocab.token_freqs: {vocab.token_freqs}')
