73bcfc91257f0beb47bc5da315d9df740fa057a1
LuoBin <2646887443@qq.com>
2025-04-24 07:45:00 +0000

    predict rnn

--- ch8/rnn_scratch/rnn_scratch.py
+++ ch8/rnn_scratch/rnn_scratch.py
@@ -1,6 +1,104 @@
 import torch
 from torch import nn
 from torch.nn import functional as F
+import collections
+import re
+import hashlib
+import os
+import requests
+
+DATA_HUB = dict()
+DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+DATA_HUB['time_machine'] = (DATA_URL + 'timemachine.txt', '090b5e7e70c295757f55df93cb0a180b9691891a')
+
+def download(name, cache_dir='data'):
+    assert name in DATA_HUB, f'{name} does not exist in {DATA_HUB}'
+    url, sha1_hash = DATA_HUB[name]
+    os.makedirs(cache_dir, exist_ok=True)
+    fname = os.path.join(cache_dir, url.split('/')[-1])
+    if os.path.exists(fname):
+        sha1 = hashlib.sha1()
+        with open(fname, 'rb') as f:
+            while True:
+                data = f.read(1048576)
+                if not data:
+                    break
+                sha1.update(data)
+        if sha1.hexdigest() == sha1_hash:
+            return fname
+    r = requests.get(url, stream=True, verify=True)
+    with open(fname, 'wb') as f:
+        f.write(r.content)
+    return fname
+
+def read_time_machine(cache_dir):
+    with open(download('time_machine', cache_dir=cache_dir), 'r') as f:
+        lines = f.readlines()
+    return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
+def tokenize(lines, token='word'):
+    if token == 'word':
+        return [line.split() for line in lines]
+    elif token == 'char':
+        return [list(line) for line in lines]
+    else:
+        print(f'Error: Unknown token type: {token}')
+
+class Vocab:
+    def __init__(self, tokens=None, min_freq=0, reserved_tokens=None):
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+        self.idx_to_token = ['<unk>'] + reserved_tokens
+        self.token_to_idx = {token: idx for idx, token in enumerate(self.idx_to_token)}
+
+        for token, freq in self._token_freqs:
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+
+    def __len__(self):
+        return len(self.idx_to_token)
+
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+
+    @property
+    def unk(self):
+        return 0
+
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+    def __iter__(self):
+        return iter(self.token_to_idx)
+
+
+def count_corpus(tokens):
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+def load_corpus_time_machine(cache_dir):
+    lines = read_time_machine(cache_dir)
+    tokens = tokenize(lines, 'char')
+    vocab = Vocab(tokens)
+    corpus = [vocab[token] for line in tokens for token in line]
+    return corpus, vocab
+
 
 def get_params(vocab_size, num_hiddens, device):
     num_inputs = num_outputs = vocab_size
@@ -40,11 +138,24 @@ class RNNModelScratch:
     def begin_state(self, batch_size, device):
         return self.init_state(batch_size, self.num_hiddens, device)
 
-X = torch.arange(10).reshape(2, 5)
+
+corpus, vocab = load_corpus_time_machine(cache_dir='../load_time_machine/data')
 
 num_hiddens = 512
-net = RNNModelScratch(28, num_hiddens, 'cuda', get_params, init_rnn_state, rnn)
-state = net.begin_state(X.shape[0], 'cuda')
-Y, new_state = net(X.to('cuda'), state)
-print(f'Y.shape: {Y.shape}')
-print(f'new_state.shape: {new_state.shape}')
+net = RNNModelScratch(len(vocab), num_hiddens, 'cuda', get_params, init_rnn_state, rnn)
+
+prefix = 'time traveller '
+num_preds = 10
+
+state = net.begin_state(batch_size=1, device='cuda')
+outputs = [vocab[prefix[0]]]
+get_input = lambda: torch.tensor([outputs[-1]], device='cuda').reshape(1, 1)
+for y in prefix[1:]:
+    _, state = net(get_input(), state)
+    outputs.append(vocab[y])
+for _ in range(num_preds):
+    y, state = net(get_input(), state)
+    outputs.append(int(y.argmax(dim=1).reshape(1)))
+
+res = ''.join([vocab.idx_to_token[i] for i in outputs])
+print(res)
